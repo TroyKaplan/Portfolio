c76b6d243c6cddb2837d5d3fe6a0e11cd4fc0bea
TroyKaplan <79412223+TroyKaplan@users.noreply.github.com>
2024-10-27 11:46:16 -0600

    upload new game

--- src/data/games.ts
+++ src/data/games.ts
@@ -72,6 +72,14 @@ const multiplayerGames: Game[] = [
         path: `${process.env.PUBLIC_URL}/assets/games/Survive/index.html`,
         instructions: 'Use arrow keys to move. Press spacebar to jump. Collect coins and play with friends.',
     },
+    {
+        id: 'RocketGame',
+        title: 'Rocket Game',
+        description: 'Hosted on dedicated server',
+        thumbnail: `${process.env.PUBLIC_URL}/assets/images/BasicSmoke.png`,
+        path: `${process.env.PUBLIC_URL}/assets/games/rocket-html5/index.html`,
+        instructions: 'Use A and D keys to rotate. Press W to fly',
+    },
     //add more
 ];
 
