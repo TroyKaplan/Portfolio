17c3f651ecb654ee05a4932b177dc86c53e9c1ba
TroyKaplan <79412223+TroyKaplan@users.noreply.github.com>
2024-10-27 16:14:33 -0600

    offload videos >100mb to youtube add lots of thumbnails

--- src/data/galleryItems.ts
+++ src/data/galleryItems.ts
@@ -37,52 +37,69 @@ const galleryItems: GalleryItem[] = [
         description: 'Random Steam networking test with destructible environment',
         thumbnail: `${process.env.PUBLIC_URL}/assets/images/POMaps.png`,
         media: [
-            { type: 'video', source: `${process.env.PUBLIC_URL}/assets/videos/POQuickShowcase.mp4` },
-            { type: 'image', source: `${process.env.PUBLIC_URL}/assets/images/POMaps.png` },
-            { type: 'image', source: `${process.env.PUBLIC_URL}/assets/images/PORocketLauncher.png` },
+            { 
+                type: 'video',
+                source: `${process.env.PUBLIC_URL}/assets/videos/POQuickShowcase.mp4`,
+                description: 'Quick showcase of the game and rocket launcher, destrustion',
+                thumbnail: `${process.env.PUBLIC_URL}/assets/images/PORocketLauncher.png`,
+            },
+            {
+                type: 'image',
+                source: `${process.env.PUBLIC_URL}/assets/images/POMaps.png`
+            },
+            {
+                type: 'image',
+                source: `${process.env.PUBLIC_URL}/assets/images/PORocketLauncher.png`
+            },
         ],
     },
     {
         id: 'item3',
         title: 'ProjectX',
-        description: 'All of my current work',
+        description: 'This project can be downloaded from steam for free',
         thumbnail: `${process.env.PUBLIC_URL}/assets/images/WolfStudios.png`,
         media: [
             {
                 type: 'video',
-                source: `${process.env.PUBLIC_URL}/assets/videos/Showcase_HungryBearAndSTEAM.mp4`,
+                source: 'https://www.youtube.com/embed/KrJxnxWIBnY',
                 description: 'Showcase of working steam leaderboards',
-                thumbnail: `${process.env.PUBLIC_URL}/assets/images/WolfStudios.png`,
+                thumbnail: `${process.env.PUBLIC_URL}/assets/images/Showcase_HungryBearSteam.png`,
             },
             {
                 type: 'video',
-                source: `${process.env.PUBLIC_URL}/assets/videos/Showcase_HUBWorld.mp4`,
+                source: 'https://www.youtube.com/embed/-ep3IX-l7Is',
                 description: 'Showcase of the HUB World',
-                thumbnail: `${process.env.PUBLIC_URL}/assets/images/WolfStudios.png`,
+                thumbnail: `${process.env.PUBLIC_URL}/assets/images/Showcase_HUBWorld.png`,
             },
             {
                 type: 'video',
-                source: `${process.env.PUBLIC_URL}/assets/videos/Showcase_HorrorGame.mp4`,
+                source: 'https://www.youtube.com/embed/R5TrJbJVaTg',
                 description: 'Showcase of a simple horror game with sequencer',
-                thumbnail: `${process.env.PUBLIC_URL}/assets/images/WolfStudios.png`,
+                thumbnail: `${process.env.PUBLIC_URL}/assets/images/Showcase_HorrorGame.png`,
             },
             {
                 type: 'video',
                 source: `${process.env.PUBLIC_URL}/assets/videos/MLAgentsVid.mp4`,
                 description: 'Showcase of a vehicles driving along a spline trained with ML models',
-                thumbnail: `${process.env.PUBLIC_URL}/assets/images/WolfStudios.png`,
+                thumbnail: `${process.env.PUBLIC_URL}/assets/images/Showcase_MLAgents.png`,
             },
             {
                 type: 'video',
-                source: `${process.env.PUBLIC_URL}/assets/videos/Showcase_BadBot.mp4`,
+                source: 'https://www.youtube.com/embed/AMmSkHH0YzU',
                 description: 'Showcase of a basic bot game with a boss',
-                thumbnail: `${process.env.PUBLIC_URL}/assets/images/WolfStudios.png`,
+                thumbnail: `${process.env.PUBLIC_URL}/assets/images/Showcase_BadBot.png`,
             },
             {
                 type: 'video',
-                source: `${process.env.PUBLIC_URL}/assets/videos/Showcase_AndroidMobile.mp4`,
+                source: 'https://www.youtube.com/embed/kiHfw46LdC0',
                 description: 'Showcase of a simple android game with over 30 levels.',
-                thumbnail: `${process.env.PUBLIC_URL}/assets/images/WolfStudios.png`,
+                thumbnail: `${process.env.PUBLIC_URL}/assets/images/Showcase_AndroidMobile.png`,
+            },
+            {
+                type: 'video',
+                source: 'https://www.youtube.com/embed/B_9g9F9qDCQ',
+                description: 'Working Prop Hunt Prototype',
+                thumbnail: `${process.env.PUBLIC_URL}/assets/images/Showcase_PropHunt.png`,
             },
             // Add descriptions and thumbnails to other media items similarly
         ],
