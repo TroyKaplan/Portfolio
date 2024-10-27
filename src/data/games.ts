6ef6bba573e22f44ae6b8a9a679ed65a857513a8
TroyKaplan <79412223+TroyKaplan@users.noreply.github.com>
2024-10-27 14:34:36 -0600

    update topdownroguelike
    for some reason it would not run remotely, refactored game to be more like a demo and rebuilt. it works locally so lets test it deployed.

--- src/data/games.ts
+++ src/data/games.ts
@@ -12,16 +12,16 @@ export interface Game {
 const gameMakerGames: Game[] = [
     {
         id: 'TopDownRogueLike',
-        title: 'Top Down Rogue Like',
-        description: 'Can you beat the boss??',
+        title: 'TopDown RogueLike',
+        description: 'Can you beat the boss?? Art by @Brtklob',
         thumbnail: `${process.env.PUBLIC_URL}/assets/images/TopdownRoguelikeThumbnail.png`,
-        path: `${process.env.PUBLIC_URL}/assets/games/TopdownRoguelike/index.html`,
+        path: `${process.env.PUBLIC_URL}/assets/games/topdownroguelike-html5/index.html`,
         instructions: 'Use WASD to move. Left-click to attack. Explore the dungeon and defeat enemies.',
     },
     {
         id: 'MainQuest2',
         title: 'Android Mobile Game',
-        description: 'Android Mobile Game, Test in browser!!',
+        description: 'Android Mobile Game, test on your phone too! (Resolution WIP)',
         thumbnail: `${process.env.PUBLIC_URL}/assets/images/MainQuest2NewThumbnail.png`,
         path: `${process.env.PUBLIC_URL}/assets/games/MainQuest2/index.html`,
         instructions: 'Use arrow keys to move. Press spacebar to jump. Collect coins and avoid obstacles.',
@@ -29,10 +29,10 @@ const gameMakerGames: Game[] = [
     {
         id: 'CoopAdventure',
         title: 'Co-op Adventure',
-        description: 'Simple same keyboard co-op',
+        description: 'Simple same keyboard co-op puzzle action game',
         thumbnail: `${process.env.PUBLIC_URL}/assets/images/CoopAdventureThumbnail.png`,
         path: `${process.env.PUBLIC_URL}/assets/games/CoopAdventure/index.html`,
-        instructions: 'Use WASD to move. Left-click to attack. Explore the dungeon and defeat enemies.',
+        instructions: 'Use WASD to move. Left/Right-click to attack. 1 and 2 to switch camera views. Arrow keys to move second player',
     },
     {
         id: 'CompMultiplayerUCCS',
@@ -40,7 +40,7 @@ const gameMakerGames: Game[] = [
         description: 'Simple same keyboard competition',
         thumbnail: `${process.env.PUBLIC_URL}/assets/images/CompMultiplayerUCCSThumbnail.png`,
         path: `${process.env.PUBLIC_URL}/assets/games/CompetitiveMultiplayerUCCS/index.html`,
-        instructions: 'Use WASD to move. Left-click to attack. Explore the dungeon and defeat enemies.',
+        instructions: 'WASD and left control for left plane, arrow keys and right control for right plane, maybe shift.',
     },
     //add more
 ];
@@ -48,8 +48,8 @@ const godotGames: Game[] = [
     {
         id: 'Pong',
         title: 'Pong',
-        description: 'Basic Pong Game',
-        thumbnail: `${process.env.PUBLIC_URL}/assets/images/BasicSmoke.png`,
+        description: 'Classic Pong',
+        thumbnail: `${process.env.PUBLIC_URL}/assets/images/Pong.png`,
         path: `${process.env.PUBLIC_URL}/assets/games/PongGodot/index.html`,
         instructions: 'Use arrow keys to move right paddle. W and S to move paddle up and down.',
     },
