69edacfabbc10ea4f93c5ef522fcaf68fea21f3e
TroyKaplan <79412223+TroyKaplan@users.noreply.github.com>
2024-10-27 12:56:41 -0600

    added SSL certs

--- src/data/games.ts
+++ src/data/games.ts
@@ -77,7 +77,7 @@ const multiplayerGames: Game[] = [
         title: 'Rocket Game',
         description: 'Hosted on dedicated server',
         thumbnail: `${process.env.PUBLIC_URL}/assets/images/BasicSmoke.png`,
-        path: `${process.env.PUBLIC_URL}/assets/games/rocket-game-client-html/index.html`,
+        path: `${process.env.PUBLIC_URL}/assets/games/rocket-client-html5/index.html`,
         instructions: 'Use A and D keys to rotate. Press W to fly',
     },
     //add more
